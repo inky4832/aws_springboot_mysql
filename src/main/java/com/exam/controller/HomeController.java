36e7fb0928d91dd161d4d7330c42512de316777b
inky4832 <inky4832@daum.net>
2026-06-29 18:17:38 +0900

    first commit2

--- src/main/java/com/exam/controller/HomeController.java
+++ src/main/java/com/exam/controller/HomeController.java
@@ -1,11 +1,14 @@
 package com.exam.controller;
 
-import jakarta.servlet.ServletContext;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import org.springframework.http.ResponseEntity;
 
 @Controller
 @SessionAttributes(value = {"my_login"})
@@ -15,6 +18,17 @@ public class HomeController {
     @GetMapping("/home")
     public String  home(){
         return "home";
+    }
+
+    @GetMapping("/health")
+    @ResponseBody
+    public ResponseEntity<String> healthCheck() {
+        try {
+            String privateIp = InetAddress.getLocalHost().getHostAddress();
+            return ResponseEntity.ok().body(privateIp + " / Success Health Check");
+        } catch (UnknownHostException e) {
+            return ResponseEntity.ok().body("Unknown Host");
+        }
     }
 
 }
